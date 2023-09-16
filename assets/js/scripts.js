5068e71d2aab6fe2e4a3fe2140bc37d5c98c946e
yakoobkhedri <yakkobkhedrii@gmail.com>
2023-09-16 12:55:28 +0330

    first commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,21 @@
+// about chart
+let aboutCircles=Array.from(document.getElementsByClassName('aboutCircles'));
+let circleContent=Array.from(document.getElementsByClassName('circleContent'));
+
+
+aboutCircles.forEach((tab) => {
+    tab.addEventListener('click', function() {
+        let tabId = tab.dataset.id;
+        circleContent.forEach((content) => {
+            let contentId = content.dataset.id;
+            if (tabId === contentId) {
+                content.classList.add('active');
+            } else {
+                content.classList.remove('active');
+            }
+        })
+    })
+})
 
 // swiper
 var swiper = new Swiper(".projects", {
